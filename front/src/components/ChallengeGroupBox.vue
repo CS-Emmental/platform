1ff7b05051a255421757bce211443bdde97f3a44
Fournier Axel <axel.fournier@supelec.fr~>
2019-11-28 11:04:35 +0100

    Add box component to display  a challeng group

---
+++ front/src/components/ChallengeGroupBox.vue
@@ -0,0 +1,42 @@
+<template>
+<emmental-box :box-props="boxProps"/>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator';
+import EmmentalBox from '@/components/EmmentalBox.vue';
+
+interface ChallengeGroupInterface {
+  title: string;
+  kebab: string;
+  id: string;
+  icon: string;
+  description: string;
+  challengesCount: number;
+}
+
+@Component({
+  name: 'ChallengeGroupBox',
+  components: {
+    EmmentalBox,
+  },
+})
+export default class EmmentalGroupBox extends Vue {
+  @Prop({
+    type: Object as () => ChallengeGroupInterface,
+    required: true,
+  })
+  public group;
+
+  private boxProps = {
+    title: this.group.title,
+    link: `/challenges/${this.group.kebab}`,
+    icon: this.group.icon,
+    subtitle: `${this.group.challengesCount} Challenges`,
+    content: this.group.description,
+  };
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
