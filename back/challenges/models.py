70f70666a52dd62ee8d666edd5c8c59e52e4f011
Sc0ra <axel.fournier@supelec.fr>
2019-12-19 15:03:04 +0100

    core model add and use in challenges

--- back/challenges/models.py
+++ back/challenges/models.py
@@ -1,11 +1,28 @@
 import time
 from uuid import uuid4
 
+from core.models import Document, from_dict_class
 
-class ChallengeCategory():
+class ChallengeCategory(Document):
+    fields = [
+        '_id',
+        'title',
+        'icon',
+        'description',
+        'created_at',
+    ]
+
+    export_fields = [
+        'category_id',
+        'title',
+        'icon',
+        'description',
+        'created_at'
+    ]
+
     def __init__(self,
-                 title: str,
                  _id: str = uuid4(),
+                 title: str = "",
                  icon: str = "fas fa-shield-alt",
                  description: str = "",
                  created_at: float = time.time()):
@@ -16,8 +33,21 @@ class ChallengeCategory():
         self.created_at = created_at
 
     @staticmethod
-    def from_dict(source):
-        return ChallengeCategory(**source)
+    def from_dict(dict_object: dict):
+        return from_dict_class(dict_object, ChallengeCategory)
 
-    def to_dict(self):
-        return vars(self)
+
+class Challenge(Document):
+    def __init__(self,
+                 _id: str = uuid4(),
+                 title: str = "",
+                 description: str = "",
+                 created_at: float = time.time()):
+        self._id = _id
+        self.title = title
+        self.description = description
+        self.created_at = created_at
+
+    @staticmethod
+    def from_dict(source):
+        return Challenge(**source)
---
+++ back/core/models.py
@@ -0,0 +1,17 @@
+class Document():
+    fields = []
+    export_fields = []
+
+    def to_dict(self):
+        return {key: getattr(self, key) for key in self.export_fields}
+        
+    @staticmethod
+    def from_dict(dict_object: dict):
+        raise NotImplementedError
+
+def from_dict_class(dict_object: dict, Class):
+    kwargs = {
+        key: value for (key, value) in dict_object.items()
+        if key in Class.fields
+    }
+    return Class(**kwargs)
