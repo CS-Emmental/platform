f34ecb86f72a3337b781da54f13cbccbb8aa77fc
Sc0ra <axel.fournier@supelec.fr>
2020-01-06 14:48:00 +0100

    protect route behind login for tests

--- back/routes/challenges.py
+++ back/routes/challenges.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, jsonify
+from flask_login import login_required
+
 from challenges.controller import get_challenge_categories
 
 challenges = Blueprint('challenges', 'challenges')
 
 @challenges.route('/challenge-categories')
+@login_required
 def get_categories():
     categories = get_challenge_categories()
     return jsonify([c.to_dict() for c in categories])
