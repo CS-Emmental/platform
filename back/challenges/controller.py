f90dfbde74919cc81b01d00ec5d92d49116394e0
lacroix_gau <gauthier.lacroix@supelec.fr>
2020-01-16 12:17:33 +0100

    def update_challenge_categorie controller

--- back/challenges/controller.py
+++ back/challenges/controller.py
@@ -3,3 +3,7 @@ from challenges.manager import ChallengeCategoriesManager
 def get_challenge_categories():
     categories = ChallengeCategoriesManager().get_all()
     return categories
+
+def update_challenge_categories(data):
+    ChallengeCategoriesManager().update_one(data)
+    return get_challenge_categories()
