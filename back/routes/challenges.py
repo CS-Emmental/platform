06970275969f649a6ee741cc8f51f47e0d2494cc
lacroix_gau <gauthier.lacroix@supelec.fr>
2020-01-16 12:19:07 +0100

    add route to update a challenge category

--- back/routes/challenges.py
+++ back/routes/challenges.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, current_app
 
-from challenges.controller import get_challenge_categories
+from challenges.controller import get_challenge_categories,update_challenge_categories
 
 challenges = Blueprint('challenges', 'challenges')
 
@@ -8,3 +8,10 @@ challenges = Blueprint('challenges', 'challenges')
 def get_categories():
     categories = get_challenge_categories()
     return jsonify([c.to_dict() for c in categories])
+
+@challenges.route('/challenge-categories/update', methods=['POST'])
+def update_categories():
+    inputs = request.json
+    current_app.logger.debug(inputs)
+    categories_updated = update_challenge_categories(inputs)
+    return jsonify([c.to_dict() for c in categories_updated])
