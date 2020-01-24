79568fda8514c622a853757cd07b3a12f18dc761
Sc0ra <axel.fournier@supelec.fr>
2020-01-24 08:41:39 +0100

    add vue-select in dependencies

--- front/src/App.vue
+++ front/src/App.vue
@@ -30,6 +30,7 @@ export default class App extends Vue {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Orbitron&display=swap');
+@import "vue-select/src/scss/vue-select.scss";
 @import '@/bulma-theme.scss';
 #app {
   font-family: 'Orbitron', Helvetica, Arial, sans-serif;
