d75a86f53c55303270ba28707b626c59f777b382
Sc0ra <axel.fournier@supelec.fr>
2020-01-24 11:13:58 +0100

    add generic modal component

---
+++ front/src/components/EmmentalModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="modal"
+    :class="{'is-active': isActive}"
+  >
+    <div class="modal-background" />
+    <div class="modal-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script  lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'EmmentalModal',
+})
+export default class EmmentalModal extends Vue {
+  @Prop({
+    type: Boolean,
+    required: true,
+  })
+  public isActive!: boolean;
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-content {
+  width: 100vw;
+  text-align: center;
+}
+</style>
