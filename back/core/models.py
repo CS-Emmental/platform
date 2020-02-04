6b601e460c5ed3fc4b53cae7e210045102fe03a9
Sc0ra <axel.fournier@supelec.fr>
2020-02-04 11:38:10 +0100

    change core models and add participations

--- back/core/models.py
+++ back/core/models.py
@@ -1,7 +1,28 @@
+import time
+from uuid import uuid4
+
 class Document():
-    fields = []
-    export_fields = []
+    fields = [
+        '_id',
+        'created_at',
+        'updated_at',
+    ]
+
+    export_fields = [
+        'created_at',
+        'updated_at',
+    ]
+
     editable_fields = []
+
+    def __init__(self,
+                 _id: str = None,
+                 created_at: int = None,
+                 updated_at:int = None,
+                ):
+        self._id = str(uuid4()) if not _id else _id
+        self.created_at = int(time.time()) if not created_at else created_at
+        self.updated_at = int(time.time()) if not updated_at else updated_at
 
     def to_dict(self):
         return {key: getattr(self, key) for key in self.export_fields}
@@ -16,6 +37,7 @@ class Document():
         inputs = {k: inputs[k] for k in self.editable_fields}
         for key in inputs:
             setattr(self, key, inputs[key])
+        setattr(self, 'updated_at', int(time.time()))
         
     @staticmethod
     def from_dict(dict_object: dict):
