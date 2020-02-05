8b54bcfd17afc32e9e1486a4b1293f1c65270d65
Theo Bonnard <theo.bonnard@supelec.fr>
2020-02-05 15:57:26 +0100

    Test of get_challenge_category

--- back/challenges/test_controller.py
+++ back/challenges/test_controller.py
@@ -1,6 +1,40 @@
-def func(x):
-    return x + 1
+"""
+Test of the controller of challenges module
+"""
+import pytest
+from challenges.controller import (
+    get_challenge_category,
+)
+from challenges.manager import ChallengeCategoriesManager
+from app import create_app
 
+class TestGetChallengeCategory():
+    """
+    Test of get_challenge_category
+    """
+
+    # Most managers inherit from MongoManager which uses the app_context.
+    app = create_app()
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            ([], []),
+            ([1], [1]),
+            (["", "string_test"], ["", "string_test"])
+        ])
+    def test_get_challenge_category(self, monkeypatch, test_input, expected):
+        """
+        Test if the function well returns what the ChallengeCategoriesManager returns
+        """
+
+        def mock_get_all(*args, **kwargs):
+            return test_input
+
+        monkeypatch.setattr(ChallengeCategoriesManager, "get_all", mock_get_all)
+
+        with self.app.app_context():
+            assert get_challenge_category() == expected
 
 def test_answer():
     assert func(3) == 5
