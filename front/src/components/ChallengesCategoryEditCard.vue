e8ad85d3fbfb7582d9721276dc8f3adbfda9f411
lacroix_gau <gauthier.lacroix@supelec.fr>
2020-02-06 12:42:49 +0100

    add Challenge Category Edit card

---
+++ front/src/components/ChallengesCategoryEditCard.vue
@@ -0,0 +1,136 @@
+<template>
+  <div
+    class="card"
+    tabindex="0"
+    @keyup.enter="$emit('save', challengeEdit)"
+    @keyup.esc="$emit('quit')"
+  >
+    <header class="card-header">
+      <p class="card-header-title">
+        Edit Category
+      </p>
+      <div class="card-header-icon">
+        <button
+          class="delete"
+          @click="$emit('quit')"
+        />
+      </div>
+    </header>
+    <div class="card-content">
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input
+            v-model="challengesCategoryEdit.title"
+            class="input"
+            type="text"
+          >
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+          <input
+            v-model="challengesCategoryEdit.description"
+            class="input"
+            type="text"
+          >
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Icon</label>
+        <div class="control">
+          <input
+            v-model="challengesCategoryEdit.icon"
+            class="input"
+            type="text"
+          >
+          <label class="label">Check name on fontawesome.com</label>
+        </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a
+        class="card-footer-item"
+        @click="$emit('quit')"
+      >Cancel</a>
+      <a
+        class="card-footer-item"
+        @click="$emit('save', challengesCategoryEdit)"
+      >Save</a>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator';
+import { State, Getter } from 'vuex-class';
+
+import vSelect from 'vue-select';
+import EmmentalRichTextEditor from '@/components/EmmentalRichTextEditor.vue';
+
+import { ChallengeCategory } from '../store/challenges/types';
+
+const namespace = 'challenges';
+
+@Component({
+  name: 'challengesCategoryEdit',
+  components: {
+    EmmentalRichTextEditor,
+    vSelect,
+  },
+})
+export default class ChallengesCategoryEdit extends Vue {
+  @Prop({
+    type: Object as () => ChallengeCategory,
+    required: false,
+  })
+  public challengeCategory: ChallengeCategory|undefined;
+
+  @State('challengeCategories', { namespace })
+  public challengeCategories: ChallengeCategory[]|undefined;
+
+  @Getter('getCategoryById', { namespace })
+  public getCategoryById!: CallableFunction;
+
+  public challengesCategoryEdit: ChallengeCategory = {
+    category_id: '',
+    title: '',
+    description: '',
+    icon: '',
+  };
+
+  public created() {
+    if (this.challengeCategory) {
+      this.challengesCategoryEdit = { ...this.challengeCategory };
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.editor-content{
+  padding: .5rem;
+  border: solid 1px #dbdbdb;
+  border-radius: 4px;
+}
+::v-deep .ProseMirror:focus {
+  outline: none;
+}
+.buttons {
+  margin-bottom: 0;
+}
+.card {
+  border-radius: 5px;
+  width: 60vw;
+  margin: auto;
+}
+.card-content {
+  height: 80vh;
+  overflow-x: auto;
+}
+.field-body .field.cost-field {
+  width: 10%;
+  flex-grow: 0;
+}
+</style>
