6c4cdf5d57c66896e9cbb3bf30dae32fe5ccdbe2
Gauthier Lacroix <gauthier.lacroix@supelec.fr>
2020-02-10 18:25:37 +0100

    add type exception

--- back/challenges/exceptions.py
+++ back/challenges/exceptions.py
@@ -9,3 +9,9 @@ class InconsistentTitleException(EmmentalException):
     external_message = "Unknown Error"
     internal_message = "Inconsistent title: title cannot be left blank"
     status_code = 500
+
+class InconsistentTypeException(EmmentalException):
+    error_code = 1
+    external_message = "Unknown Error"
+    internal_message = "Inconsistent type: an input did not respect the right type"
+    status_code = 500
