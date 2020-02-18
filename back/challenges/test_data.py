39746c738c2406cdea895846eb0429c1a0e13a37
Gauthier Lacroix <gauthier.lacroix@supelec.fr>
2020-02-18 17:46:21 +0100

    beautify

--- back/challenges/test_data.py
+++ back/challenges/test_data.py
@@ -1,14 +1,16 @@
 data_challenge_category_legit_args = [
     (
-        {"_id": "a", "title": "a", "icon": "a","description":"a","created_at": 0,"updated_at": 0},
-        {"_id": "a", "title": "a", "icon": "a","description":"a","created_at": 0,"updated_at": 0},
+        {"_id": "a", "title": "a", "icon": "a",
+         "description": "a", "created_at": 0, "updated_at": 0},
+        {"_id": "a", "title": "a", "icon": "a",
+         "description": "a", "created_at": 0, "updated_at": 0},
     ),
     (
         {
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
             "icon": "icon",
-            "description":"description",
+            "description": "description",
             "created_at": 1581002681,
             "updated_at": 1581002681,
         },
@@ -16,7 +18,7 @@ data_challenge_category_legit_args = [
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
             "icon": "icon",
-            "description":"description",
+            "description": "description",
             "created_at": 1581002681,
             "updated_at": 1581002681,
         },
@@ -26,7 +28,7 @@ data_challenge_category_legit_args = [
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
             "icon": "icon",
-            "description":"description",
+            "description": "description",
             "created_at": 1581002681,
             "updated_at": 1681002681,
         },
@@ -34,7 +36,7 @@ data_challenge_category_legit_args = [
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
             "icon": "icon",
-            "description":"description",
+            "description": "description",
             "created_at": 1581002681,
             "updated_at": 1681002681,
         },
@@ -42,34 +44,119 @@ data_challenge_category_legit_args = [
 ]
 
 data_challenge_category_error_time = [
-    ({"_id": None, "title": "title", "icon": "icon" ,"description": "description","created_at": 10,"updated_at": 5}),
-    ({"_id": None, "title": "title", "icon": "icon" ,"description": "description","created_at": None,"updated_at": 10})
+    (
+        {
+            "_id": None,
+            "title": "title",
+            "icon": "icon",
+            "description": "description",
+            "created_at": 10,
+            "updated_at": 5
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "title",
+            "icon": "icon",
+            "description": "description",
+            "created_at": None,
+            "updated_at": 10
+        }
+    )
 ]
 
 data_challenge_category_error_title = [
-    ({"_id": None, "title": "", "icon": "icon" ,"description": "description","created_at": 0,"updated_at": 0}),
+    (
+        {
+            "_id": None,
+            "title": "",
+            "icon": "icon",
+            "description": "description",
+            "created_at": 0,
+            "updated_at": 0
+        }
+    ),
 ]
-data_challenge_category_error_type= [
-    ({"_id": None, "title": None, "icon": "icon" ,"description": "description","created_at": 0,"updated_at": "z"}),
-    ({"_id": None, "title": "title", "icon": None ,"description": "description","created_at": 0,"updated_at": "z"}),
-    ({"_id": None, "title": "title", "icon": "icon" ,"description": None,"created_at": 0,"updated_at": "z"}),
-    ({"_id": None, "title": 1, "icon": 1 ,"description": 1,"created_at": 0,"updated_at": "z"})
+data_challenge_category_error_type = [
+    (
+        {
+            "_id": None,
+            "title": None,
+            "icon": "icon",
+            "description": "description",
+            "created_at": 0,
+            "updated_at": "z"
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "title",
+            "icon": None,
+            "description": "description",
+            "created_at": 0,
+            "updated_at": "z"
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "title",
+            "icon": "icon",
+            "description": None,
+            "created_at": 0,
+            "updated_at": "z"
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": 1,
+            "icon": 1,
+            "description": 1,
+            "created_at": 0,
+            "updated_at": "z"
+        }
+    )
 ]
 
 data_challenge_legit_args = [
     (
-        {"_id": "a", "title": "a","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": 0,"updated_at": 0},
-        {"_id": "a", "title": "a","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": 0,"updated_at": 0},
+        {
+            "_id": "a",
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": 0,
+            "updated_at": 0
+        },
+        {
+            "_id": "a",
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": 0,
+            "updated_at": 0
+        },
     ),
     (
         {
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
-            "description":"description",
-            "summary":"summary",
-            "category_id":"category",
-            "total_points":0,
-            "flags":[],
+            "description": "description",
+            "summary": "summary",
+            "category_id": "category",
+            "total_points": 0,
+            "flags": [],
             "hints":[],
             "created_at": 1581002681,
             "updated_at": 1581002681,
@@ -77,11 +164,11 @@ data_challenge_legit_args = [
         {
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
-            "description":"description",
-            "summary":"summary",
-            "category_id":"category",
-            "total_points":0,
-            "flags":[],
+            "description": "description",
+            "summary": "summary",
+            "category_id": "category",
+            "total_points": 0,
+            "flags": [],
             "hints":[],
             "created_at": 1581002681,
             "updated_at": 1581002681,
@@ -91,11 +178,11 @@ data_challenge_legit_args = [
         {
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
-            "description":"description",
-            "summary":"summary",
-            "category_id":"category",
-            "total_points":0,
-            "flags":[],
+            "description": "description",
+            "summary": "summary",
+            "category_id": "category",
+            "total_points": 0,
+            "flags": [],
             "hints":[],
             "created_at": 1581002681,
             "updated_at": 1681002681,
@@ -103,11 +190,11 @@ data_challenge_legit_args = [
         {
             "_id": "c853fd69-e222-4e07-a261-efe1791aa542",
             "title": "title",
-            "description":"description",
-            "summary":"summary",
-            "category_id":"category",
-            "total_points":0,
-            "flags":[],
+            "description": "description",
+            "summary": "summary",
+            "category_id": "category",
+            "total_points": 0,
+            "flags": [],
             "hints":[],
             "created_at": 1581002681,
             "updated_at": 1681002681,
@@ -116,30 +203,227 @@ data_challenge_legit_args = [
 ]
 
 data_challenge_error_time = [
-        ({"_id": None, "title": "a","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": 10,"updated_at": 5}),
-        ({"_id": None, "title": "a","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": 5}),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": 10,
+            "updated_at": 5
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": None,
+            "updated_at": 5
+        }
+    ),
 ]
 
 data_challenge_error_title = [
-        {"_id": None, "title": "","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": None},
+    {
+        "_id": None,
+        "title": "",
+        "description": "a",
+        "summary": "a",
+        "category_id": "a",
+        "total_points": 0,
+        "flags": [],
+        "hints":[],
+        "created_at": None,
+        "updated_at": None
+    },
 ]
-data_challenge_error_type= [
-    ({"_id": None, "title": None,"description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":None,"summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":"a","summary":1,"category_id":"a","total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":"a","summary":"a","category_id":[],"total_points":0,"flags":[],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":"a","summary":"a","category_id":"","total_points":"points","flags":[],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":"a","summary":"a","category_id":"","total_points":0,"flags":"[]","hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "a","description":"a","summary":"a","category_id":"","total_points":0,"flags":[],"hints":"[]","created_at": None,"updated_at": None}),
+data_challenge_error_type = [
+    (
+        {
+            "_id": None,
+            "title": None,
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": None,
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": 1,
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": [],
+            "total_points":0,
+            "flags":[],
+            "hints":[],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "",
+            "total_points": "points",
+            "flags": [],
+            "hints":[],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "",
+            "total_points": 0,
+            "flags": "[]",
+            "hints": [],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "a",
+            "description": "a",
+            "summary": "a",
+            "category_id": "",
+            "total_points": 0,
+            "flags": [],
+            "hints":"[]",
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
 ]
 
-data_challenge_error_hints=[
-    ({"_id": None, "title": "None","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[{"cost":0.2,"text":""},{"cost":0.9,"text":""}],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "None","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[],"hints":[{"cost":-0.2,"text":""},{"cost":0.2,"text":""}],"created_at": None,"updated_at": None}),
+data_challenge_error_hints = [
+    (
+        {
+            "_id": None,
+            "title": "None",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[{"cost": 0.2, "text": ""}, {"cost": 0.9, "text": ""}],
+            "created_at": None,
+            "updated_at": None}),
+    (
+        {
+            "_id": None,
+            "title": "None",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [],
+            "hints":[{"cost": -0.2, "text": ""}, {"cost": 0.2, "text": ""}],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
 ]
 
-data_challenge_error_flags=[
-    ({"_id": None, "title": "None","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[{"reward":0.2,"value":"","text":""},{"reward":-0.2,"value":"","text":""}],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "None","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[{"reward":0.9,"value":"","text":""},{"reward":0.2,"value":"","text":""}],"hints":[],"created_at": None,"updated_at": None}),
-    ({"_id": None, "title": "None","description":"a","summary":"a","category_id":"a","total_points":0,"flags":[{"reward":0.9,"value":"","text":""}],"hints":[],"created_at": None,"updated_at": None}),
+data_challenge_error_flags = [
+    (
+        {
+            "_id": None,
+            "title": "None",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [
+                {"reward": 0.2, "value": "", "text": ""},
+                {"reward": -0.2, "value": "", "text": ""},
+            ],
+            "hints": [],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "None",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [
+                {"reward": 0.9, "value": "", "text": ""},
+                {"reward": 0.2, "value": "", "text": ""},
+            ],
+            "hints": [],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
+    (
+        {
+            "_id": None,
+            "title": "None",
+            "description": "a",
+            "summary": "a",
+            "category_id": "a",
+            "total_points": 0,
+            "flags": [{"reward": 0.9, "value": "", "text": ""}],
+            "hints": [],
+            "created_at": None,
+            "updated_at": None
+        }
+    ),
 ]
