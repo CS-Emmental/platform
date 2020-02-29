31ca2051cef0669807494960c4ced772228cae8f
Gauthier Lacroix <gauthier.lacroix@supelec.fr>
2020-02-29 10:56:20 +0100

    add test controller for challenge category

--- back/challenge_categories/test_controller.py
+++ back/challenge_categories/test_controller.py
@@ -2,7 +2,12 @@
 Test of the controller of challenges module
 """
 import pytest
-from challenge_categories.controller import get_challenge_categories
+from challenge_categories.controller import (
+    get_challenge_categories,
+    update_challenge_category,
+    remove_challenge_category,
+    insert_challenge_category,
+)
 from challenge_categories.manager import ChallengeCategoriesManager
 from app import create_app
 
@@ -30,3 +35,92 @@ class TestGetChallengeCategories:
 
         with self.app.app_context():
             assert get_challenge_categories() == expected
+
+class TestUpdateChallengeCategory:
+    """
+    Test of update_challenge_category
+    """
+
+    app = create_app(testing=True)
+
+    @pytest.mark.parametrize(
+        "database_input,test_input,expected",
+        [
+            ({"key": "value"}, {"key": "new_value"}, {"key": "new_value"}),
+            ({"key": "value"}, {}, {"key": "value"}),
+            (
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "new_value1", "key2": "value2"},
+                {"key1": "new_value1", "key2": "value2"},
+            ),
+            (
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "new_value1"},
+                {"key1": "new_value1", "key2": "value2"},
+            ),
+            (
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "new_value1", "key2": "new_value2"},
+                {"key1": "new_value1", "key2": "new_value2"},
+            ),
+        ],
+    )
+    def test_update_challenge_category(
+        self, monkeypatch, database_input, test_input, expected
+    ):
+        def mock_get(*args, **kwargs):
+            return database_input
+
+        def mock_update(*args, **kwargs):
+            return None
+
+        monkeypatch.setattr(ChallengeCategoriesManager, "get", mock_get)
+        monkeypatch.setattr(ChallengeCategoriesManager, "update_one", mock_update)
+
+        with self.app.app_context():
+            assert update_challenge_category("id", test_input) == expected
+
+
+class TestRemoveChallengeCategory:
+    """
+    Test of remove_challenge_category
+    """
+
+    app = create_app(testing=True)
+
+    @pytest.mark.parametrize(
+        "database_input,expected", [({"key": "value"}, {"n": 1, "ok": 1.0})]
+    )
+    def test_remove_challenge_category(self, monkeypatch, database_input, expected):
+        def mock_get(*args, **kwargs):
+            return database_input
+
+        def mock_remove(*args, **kwargs):
+            return {"n": 1, "ok": 1.0}
+
+        monkeypatch.setattr(ChallengeCategoriesManager, "get", mock_get)
+        monkeypatch.setattr(ChallengeCategoriesManager, "remove_one", mock_remove)
+
+        with self.app.app_context():
+            assert remove_challenge_category("id") == expected
+
+
+class TestInsertChallengeCategory:
+    """
+    Test of insert_challenge_category
+    """
+
+    app = create_app(testing=True)
+
+    @pytest.mark.parametrize(
+        "test_input,expected", [({"title": "value"}, {"title": "value"})]
+    )
+    def test_insert_challenge_category(self, monkeypatch, test_input, expected):
+        def mock_insert(*args, **kwargs):
+            return None
+
+        monkeypatch.setattr(ChallengeCategoriesManager, "insert_one", mock_insert)
+
+        with self.app.app_context():
+            assert insert_challenge_category(test_input).title == expected["title"]
+
