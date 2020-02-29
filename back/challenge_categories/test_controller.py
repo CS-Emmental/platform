017bfe48499fb8a1f70fe782601531f8a50e8db7
Gauthier Lacroix <gauthier.lacroix@supelec.fr>
2020-02-29 11:13:37 +0100

    add data from test_controller in test_data

--- back/challenge_categories/test_controller.py
+++ back/challenge_categories/test_controller.py
@@ -10,6 +10,13 @@ from challenge_categories.controller import (
 )
 from challenge_categories.manager import ChallengeCategoriesManager
 from app import create_app
+from challenge_categories.test_data import (
+    data_controller_get,
+    data_controller_update,
+    data_challenge_category_legit_args,
+    data_controller_remove,
+    data_controller_insert,
+)
 
 
 class TestGetChallengeCategories:
@@ -20,9 +27,7 @@ class TestGetChallengeCategories:
     # Most managers inherit from MongoManager which uses the app_context.
     app = create_app(testing=True)
 
-    @pytest.mark.parametrize(
-        "test_input,expected", [([], []), ([1], [1]), (["", "string_test"], ["", "string_test"])],
-    )
+    @pytest.mark.parametrize("test_input,expected", data_controller_get)
     def test_get_challenge_categories(self, monkeypatch, test_input, expected):
         """
         Test if the function well returns what the ChallengeCategoriesManager returns
@@ -36,6 +41,7 @@ class TestGetChallengeCategories:
         with self.app.app_context():
             assert get_challenge_categories() == expected
 
+
 class TestUpdateChallengeCategory:
     """
     Test of update_challenge_category
@@ -44,26 +50,7 @@ class TestUpdateChallengeCategory:
     app = create_app(testing=True)
 
     @pytest.mark.parametrize(
-        "database_input,test_input,expected",
-        [
-            ({"key": "value"}, {"key": "new_value"}, {"key": "new_value"}),
-            ({"key": "value"}, {}, {"key": "value"}),
-            (
-                {"key1": "value1", "key2": "value2"},
-                {"key1": "new_value1", "key2": "value2"},
-                {"key1": "new_value1", "key2": "value2"},
-            ),
-            (
-                {"key1": "value1", "key2": "value2"},
-                {"key1": "new_value1"},
-                {"key1": "new_value1", "key2": "value2"},
-            ),
-            (
-                {"key1": "value1", "key2": "value2"},
-                {"key1": "new_value1", "key2": "new_value2"},
-                {"key1": "new_value1", "key2": "new_value2"},
-            ),
-        ],
+        "database_input,test_input,expected", data_controller_update
     )
     def test_update_challenge_category(
         self, monkeypatch, database_input, test_input, expected
@@ -88,9 +75,7 @@ class TestRemoveChallengeCategory:
 
     app = create_app(testing=True)
 
-    @pytest.mark.parametrize(
-        "database_input,expected", [({"key": "value"}, {"n": 1, "ok": 1.0})]
-    )
+    @pytest.mark.parametrize("database_input,expected", data_controller_remove)
     def test_remove_challenge_category(self, monkeypatch, database_input, expected):
         def mock_get(*args, **kwargs):
             return database_input
@@ -112,9 +97,7 @@ class TestInsertChallengeCategory:
 
     app = create_app(testing=True)
 
-    @pytest.mark.parametrize(
-        "test_input,expected", [({"title": "value"}, {"title": "value"})]
-    )
+    @pytest.mark.parametrize("test_input,expected", data_controller_insert)
     def test_insert_challenge_category(self, monkeypatch, test_input, expected):
         def mock_insert(*args, **kwargs):
             return None
@@ -123,4 +106,3 @@ class TestInsertChallengeCategory:
 
         with self.app.app_context():
             assert insert_challenge_category(test_input).title == expected["title"]
-
