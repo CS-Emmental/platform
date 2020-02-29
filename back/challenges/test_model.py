588d77b20e10df8b00ad3048d331ae848619444e
Gauthier Lacroix <gauthier.lacroix@supelec.fr>
2020-02-29 10:47:39 +0100

    refacto challenge model test

--- back/challenges/test_model.py
+++ back/challenges/test_model.py
@@ -17,7 +17,19 @@ from core.exceptions import (
     EmmentalEmptyFieldException,
     InconsistentDateException,
 )
-
+def get_challenge(test_input):
+    return Challenge(
+        _id=test_input["_id"],
+        created_at=test_input["created_at"],
+        updated_at=test_input["updated_at"],
+        description=test_input["description"],
+        title=test_input["title"],
+        summary=test_input["summary"],
+        hints=test_input["hints"],
+        flags=test_input["flags"],
+        total_points=test_input["total_points"],
+        category_id=test_input["category_id"],
+    )
 
 class TestInit:
     """
@@ -43,18 +55,7 @@ class TestInit:
 
     @pytest.mark.parametrize("test_input,expected", data_challenge_legit_args)
     def test_challenge_legit_args(self, test_input, expected):
-        challenge = Challenge(
-            _id=test_input["_id"],
-            created_at=test_input["created_at"],
-            updated_at=test_input["updated_at"],
-            description=test_input["description"],
-            title=test_input["title"],
-            summary=test_input["summary"],
-            hints=test_input["hints"],
-            flags=test_input["flags"],
-            total_points=test_input["total_points"],
-            category_id=test_input["category_id"],
-        )
+        challenge = get_challenge(test_input)
 
         assert challenge._id == expected["_id"]
         assert challenge.created_at == expected["created_at"]
@@ -69,79 +70,29 @@ class TestInit:
     @pytest.mark.parametrize("test_input", data_challenge_error_time)
     def test_error_time(self, test_input):
         with pytest.raises(InconsistentDateException):
-            challenge = Challenge(
-                _id=test_input["_id"],
-                created_at=test_input["created_at"],
-                updated_at=test_input["updated_at"],
-                description=test_input["description"],
-                title=test_input["title"],
-                summary=test_input["summary"],
-                hints=test_input["hints"],
-                flags=test_input["flags"],
-                total_points=test_input["total_points"],
-                category_id=test_input["category_id"],
-            )
+            challenge = get_challenge(test_input)
+
 
     @pytest.mark.parametrize("test_input", data_challenge_error_title)
     def test_error_title(self, test_input):
         with pytest.raises(EmmentalEmptyFieldException):
-            challenge = Challenge(
-                _id=test_input["_id"],
-                created_at=test_input["created_at"],
-                updated_at=test_input["updated_at"],
-                description=test_input["description"],
-                title=test_input["title"],
-                summary=test_input["summary"],
-                hints=test_input["hints"],
-                flags=test_input["flags"],
-                total_points=test_input["total_points"],
-                category_id=test_input["category_id"],
-            )
+            challenge = get_challenge(test_input)
+
 
     @pytest.mark.parametrize("test_input", data_challenge_error_type)
     def test_error_type(self, test_input):
         with pytest.raises(EmmentalTypeException):
-            challenge = Challenge(
-                _id=test_input["_id"],
-                created_at=test_input["created_at"],
-                updated_at=test_input["updated_at"],
-                description=test_input["description"],
-                title=test_input["title"],
-                summary=test_input["summary"],
-                hints=test_input["hints"],
-                flags=test_input["flags"],
-                total_points=test_input["total_points"],
-                category_id=test_input["category_id"],
-            )
+            challenge = get_challenge(test_input)
+
 
     @pytest.mark.parametrize("test_input", data_challenge_error_hints)
     def test_error_hints(self, test_input):
         with pytest.raises(EmmentalHintsException):
-            challenge = Challenge(
-                _id=test_input["_id"],
-                created_at=test_input["created_at"],
-                updated_at=test_input["updated_at"],
-                description=test_input["description"],
-                title=test_input["title"],
-                summary=test_input["summary"],
-                hints=test_input["hints"],
-                flags=test_input["flags"],
-                total_points=test_input["total_points"],
-                category_id=test_input["category_id"],
-            )
+            challenge = get_challenge(test_input)
+
 
     @pytest.mark.parametrize("test_input", data_challenge_error_flags)
     def test_error_flags(self, test_input):
         with pytest.raises(EmmentalFlagsException):
-            challenge = Challenge(
-                _id=test_input["_id"],
-                created_at=test_input["created_at"],
-                updated_at=test_input["updated_at"],
-                description=test_input["description"],
-                title=test_input["title"],
-                summary=test_input["summary"],
-                hints=test_input["hints"],
-                flags=test_input["flags"],
-                total_points=test_input["total_points"],
-                category_id=test_input["category_id"],
-            )
+            challenge = get_challenge(test_input)
+
