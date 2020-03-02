d38b8726cbac2216a28c8315dbb8e1e12b692b05
Theo Bonnard <theo.bonnard@supelec.fr>
2020-03-02 10:01:15 +0100

    Refacto IncorrectCredentialsException

--- back/users/exceptions.py
+++ back/users/exceptions.py
@@ -2,6 +2,11 @@ from core.exceptions import EmmentalException
 
 
 class IncorrectCredentialsException(EmmentalException):
-    error_code = 1
-    error_message = "Incorrect credentials"
-    status_code = 500
+    def __init__(self, error_code: int):
+        self.error_code = error_code
+        self.external_message = "Incorrect credentials"
+        self.internal_message = "Given credentials do not match"
+        status_code = 500
+
+    def __str__(self):
+        return self.internal_message
