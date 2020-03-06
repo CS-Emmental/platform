bb992faf98f7dc53f4dcd4a37252eceaf284de74
Theo Bonnard <theo.bonnard@supelec.fr>
2020-03-06 15:06:18 +0100

    challenge_category update check if title_slug is unique
    Before inserting the object in db

--- back/challenge_categories/controller.py
+++ back/challenge_categories/controller.py
@@ -1,5 +1,6 @@
 from challenge_categories.manager import ChallengeCategoriesManager
 from challenge_categories.model import ChallengeCategory
+from core.exceptions import EmmentalNotUniqueException
 
 
 def get_challenge_categories():
@@ -8,10 +9,20 @@ def get_challenge_categories():
 
 
 def update_challenge_category(category_id: str, inputs: dict):
-    category_updated = ChallengeCategoriesManager().get(category_id)
-    category_updated.update(inputs)
-    ChallengeCategoriesManager().update_one(category_updated)
-    return category_updated
+    category_manager = ChallengeCategoriesManager()
+
+    category_to_update = category_manager.get(category_id)
+    category_to_update.update(inputs)
+
+    same_title_slug_categories = category_manager.get_query(
+        {"title_slug": category_to_update.title_slug}
+    )
+    for challenge_category in same_title_slug_categories:
+        if challenge_category.category_id != category_to_update.category_id:
+            raise EmmentalNotUniqueException(error_code=26)
+
+    category_manager.update_one(category_to_update)
+    return category_to_update
 
 
 def remove_challenge_category(category_id: str):
