5e6b20d2fda4dcf8143d1fa2e6a89a4d5be4f1ca
Theo Bonnard <theo.bonnard@supelec.fr>
2020-03-06 17:19:45 +0100

    Block create and update if same title slug in same category

--- back/challenges/controller.py
+++ back/challenges/controller.py
@@ -2,6 +2,7 @@ from flask_login import current_user
 
 from challenges.manager import ChallengesManager
 from challenges.model import Challenge
+from core.exceptions import EmmentalNotUniqueException
 
 
 def get_all_challenges():
@@ -17,17 +18,41 @@ def get_all_challenges():
 
 
 def update_challenge(challenge_id: str, inputs: dict):
-    challenge_updated = ChallengesManager().get(challenge_id)
-    challenge_updated.update(inputs)
+    challenge_manager = ChallengesManager()
 
-    ChallengesManager().update_one(challenge_updated)
-    return challenge_updated
+    challenge_to_update = challenge_manager.get(challenge_id)
+    challenge_to_update.update(inputs)
+
+    same_title_slug_challenges = challenge_manager.get_query(  # list
+        {
+            "title_slug": challenge_to_update.title_slug,
+            "category_id": challenge_to_update.category_id,
+        }
+    )
+    for challenge in same_title_slug_challenges:
+        if challenge.challenge_id != challenge_to_update.challenge_id:
+            raise EmmentalNotUniqueException(error_code=28)
+
+    challenge_manager.update_one(challenge_to_update)
+    return challenge_to_update
 
 
 def insert_challenge(inputs: dict):
-    challenge_inserted = Challenge(**inputs)
-    ChallengesManager().insert_one(challenge_inserted)
-    return challenge_inserted
+    challenge_manager = ChallengesManager()
+
+    challenge_to_insert = Challenge(**inputs)
+
+    same_title_slug_challenges = challenge_manager.count(  # count
+        {
+            "title_slug": challenge_to_insert.title_slug,
+            "category_id": challenge_to_insert.category_id,
+        }
+    )
+    if same_title_slug_challenges != 0:
+        raise EmmentalNotUniqueException(error_code=29)
+
+    challenge_manager.insert_one(challenge_to_insert)
+    return challenge_to_insert
 
 
 def remove_challenge(challenge_id: str):
